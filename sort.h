da343fff5b115d3ad23fadb285ead7b84b2adb88
hyperayoub <bouagnaayoub@gmail.com>
2023-11-21 10:33:10 -0800

    sorts an array of integers in ascending order using the Bubble sort

--- sort.h
+++ sort.h
@@ -50,29 +50,9 @@ typedef struct card_s
  * @prev: Pointer to the previous node of the list
  * @next: Pointer to the next node of the list
  */
- /
-
  
-/* data structures for sorting a deck of cards */
-typedef struct deck_node_s
-{
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
-} deck_node_t;
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-// void insertion_sort_list(listint_t **list);
-// void selection_sort(int *array, size_t size);
-// void quick_sort(int *array, size_t size);
-// void shell_sort(int *array, size_t size);
-// void cocktail_sort_list(listint_t **list);
-// void counting_sort(int *array, size_t size);
-// void merge_sort(int *array, size_t size);
-// void heap_sort(int *array, size_t size);
-// void radix_sort(int *array, size_t size);
-// void bitonic_sort(int *array, size_t size);
-// void quick_sort_hoare(int *array, size_t size);
-// void sort_deck(deck_node_t **deck);
+#endif
