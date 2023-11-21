089df7d39b383a78ff0bcc8b51936f6b929c1464
hyperayoub <bouagnaayoub@gmail.com>
2023-11-21 11:22:47 -0800

    sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,35 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
-* bubble_sort - function that sorts ana array
-* @array: the integer to be sorted 
-* @size: size of the array
-*/
-
+ * bubble_sort - Sorts an array of integers in ascending order
+ *
+ * @array: Array of integer
+ * @size: Size of the array
+ *
+ * Return: void
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
+	size_t r;
+	int _tmp, _sorted;
 
-    for(i = 0; i < size; i++)
-           for (j = 0; j < size - 1; j++)
-                   if(array[j] > array[j + 1])
-                   {
-                          temp = array[j];
-                          array[j] = array[j + 1];
-                          array[j + 1] = temp;
-                          print_array(array, size);
-                   }
+	if (!array || size == 0)
+		return;
+
+	do {
+		_sorted = 1;
+		for (r = 0; r < size - 1; r++)
+		{
+			if (array[r] > array[r + 1])
+			{
+				_sorted = 0;
+				_tmp = array[r];
+				array[r] = array[r + 1];
+				array[r + 1] = _tmp;
+				print_array(array, size);
+			}
+		}
+
+	} while (_sorted == 0);
 }
-
-    
-
-
-
-
-
-
--- 0-main.c
+++ 0-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-
 /**
  * main - Entry point
  *
@@ -11,7 +10,6 @@ int main(void)
 {
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
-
     print_array(array, n);
     printf("\n");
     bubble_sort(array, n);
@@ -19,3 +17,4 @@ int main(void)
     print_array(array, n);
     return (0);
 }
+
