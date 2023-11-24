61c51e72bec0490c4e4e556c6c9725367d492494
hyperayoub <bouagnaayoub@gmail.com>
2023-11-24 12:06:43 -0800

    bitonic_sort

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 /**
- * compare_bitonic - Compare and swap elements in a bitonic sequence.
+ * cmp_bitonic - Compare and swap elements in a bitonic sequence.
  * @up: Direction of sorting.
  * @array: Sub-array to sort.
  * @size: Size of the sub-array.
  */
-void compare_bitonic(char up, int *array, size_t size)
+void cmp_bitonic(char up, int *array, size_t size)
 {
-	size_t i, half_size;
+	size_t i, hsz;
 	int swap;
 
-	half_size = size / 2;
-	for (i = 0; i < half_size; i++)
+	hsz = size / 2;
+	for (i = 0; i < hsz; i++)
 	{
-		if ((array[i] > array[i + half_size]) == up)
+		if ((array[i] > array[i + hsz]) == up)
 		{
 			swap = array[i];
-			array[i] = array[i + half_size];
-			array[i + half_size] = swap;
+			array[i] = array[i + hsz];
+			array[i + hsz] = swap;
 		}
 	}
 }
@@ -35,45 +35,45 @@ void merge_bitonic(char up, int *array, size_t size)
 	if (size < 2)
 		return;
 
-	compare_bitonic(up, array, size);
+	cmp_bitonic(up, array, size);
 	merge_bitonic(up, array, size / 2);
 	merge_bitonic(up, array + (size / 2), size / 2);
 }
 
 /**
- * recursive_bitonic_sort - Recursively apply the bitonic sort algorithm.
+ * rec_bitonic_sort - Recursively apply the bitonic sort algorithm.
  * @up: Direction of sorting.
  * @array: Sub-array to sort.
  * @size: Size of the sub-array.
- * @total_size: Total size of the original array.
+ * @ts: Total size of the original array.
  */
-void recursive_bitonic_sort(char up, int *array, size_t size, size_t total_size)
+void rec_bitonic(char up, int *array, size_t size, size_t ts)
 {
 	if (size < 2)
 		return;
 
-	printf("Merging [%lu/%lu] (%s):\n", size, total_size, (up == 1) ? "UP" : "DOWN");
+	printf("Merging [%lu/%lu] (%s):\n", size, ts, (up == 1) ? "UP" : "DOWN");
 	print_array(array, size);
 
-	recursive_bitonic_sort(1, array, size / 2, total_size);
-	recursive_bitonic_sort(0, array + (size / 2), size / 2, total_size);
+	rec_bitonic(1, array, size / 2, ts);
+	rec_bitonic(0, array + (size / 2), size / 2, ts);
 
 	merge_bitonic(up, array, size);
 
-	printf("Result [%lu/%lu] (%s):\n", size, total_size, (up == 1) ? "UP" : "DOWN");
+	printf("Result [%lu/%lu] (%s):\n", size, ts, (up == 1) ? "UP" : "DOWN");
 	print_array(array, size);
 }
 
 /**
- * bitonic_sort - Sort an array in ascending order using the bitonic sort algorithm.
- * @array: Array to sort.
- * @size: Size of the array.
+ * bitonic_sort - Sort an array.
+ * @array: Array sort.
+ * @size: Size of array.
  */
 void bitonic_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
 
-	recursive_bitonic_sort(1, array, size, size);
+	rec_bitonic(1, array, size, size);
 }
 
