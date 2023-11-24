777a4cdf12b2755717f328e303d3b3c41dc4c9d7
hyperayoub <bouagnaayoub@gmail.com>
2023-11-24 01:30:27 -0800

    sort_algo

--- sort.h
+++ sort.h
@@ -21,21 +21,17 @@ typedef struct listint_s
 } listint_t;
 
 
-/* print_array, and print_list functions*/
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
