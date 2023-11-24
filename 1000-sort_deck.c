35f661d1c3e93b589d0cae40f7b07bcf8dda7a00
hyperayoub <bouagnaayoub@gmail.com>
2023-11-24 13:02:43 -0800

    Dealer/card/deck

---
+++ 1000-sort_deck.c
@@ -0,0 +1,95 @@
+#include "deck.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+* get_card - Gets the relative value
+* @str: Value of the card
+*
+* Return: Relative value of the card (0 through 12)
+*/
+int get_card(const char *str)
+{
+	int i;
+	char *array[13] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9",
+			"10", "Jack", "Queen", "King"};
+
+	for (i = 0; i < 13; i++)
+	{
+		if (strcmp(str, array[i]) == 0)
+		{
+			return (i);
+		}
+	}
+	exit(1);
+}
+
+/**
+* swap_node - Swaps a node within a doubly linked list
+* @list: Double pointer to the list
+* @node: Node to be swapped
+*
+* Return: void
+*/
+void swap_node(deck_node_t **list, deck_node_t *node)
+{
+	node->next->prev = node->prev;
+	if (node->prev)
+		node->prev->next = node->next;
+	else
+		*list = node->next;
+	node->prev = node->next;
+	node->next = node->next->next;
+	node->prev->next = node;
+	if (node->next)
+		node->next->prev = node;
+}
+
+/**
+* sort_deck - Sorts a linked list deck of cards using the Bubble Sort algorithm
+* @deck: Double pointer to the deck to sort
+*
+* Return: void
+*/
+void sort_deck(deck_node_t **deck)
+{
+	char swapped = 1, ca1, ca2;
+	deck_node_t *current;
+
+	if (deck == NULL || *deck == NULL || (*deck)->next == NULL)
+		return;
+	current = *deck;
+	while (swapped != 0)
+	{
+		swapped = 0;
+		while (current->next != NULL)
+		{
+			ca1 = get_card(current->card->value) + 13 * current->card->kind;
+			ca2 = get_card(current->next->card->value) + 13 * current->next->card->kind;
+			if (ca1 > ca2)
+			{
+				swap_node(deck, current);
+				swapped = 1;
+			}
+			else
+				current = current->next;
+		}
+		if (swapped == 0)
+			break;
+		swapped = 0;
+		while (current->prev != NULL)
+		{
+			ca1 = get_card(current->card->value) + 13 * current->card->kind;
+			ca2 = get_card(current->prev->card->value) + 13 * current->prev->card->kind;
+			if (ca1 < ca2)
+			{
+				swap_node(deck, current->prev);
+				swapped = 1;
+			}
+			else
+				current = current->prev;
+		}
+	}
+}
+
