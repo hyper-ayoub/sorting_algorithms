039ee65871bea0c7007ec0dd0e75a52b806adf9c
hyperayoub <bouagnaayoub@gmail.com>
2023-11-24 11:09:08 -0800

    heap_sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,70 +1,78 @@
 #include "sort.h"
+
 /**
- * check - swiftdown check
- * @array: pointer to array
- * @size: size of the pointer
- * @size_i: original size of the array
- * @i: index as a root of the tree
- *
-**/
-void check(int *array, size_t size_init, size_t size, size_t i)
+ * adjust_heap - Adjusts the heap structure starting from a given root.
+ * @array: The array representing the heap.
+ * @root: The root index of the heap.
+ * @end: The last index of the heap.
+ * @size: The size of the array.
+ */
+void adjust_heap(int *array, size_t root, size_t end, size_t size)
 {
+	size_t left, right, swap;
+	int temp;
 
-	int n, branch1, branch2;
-	size_t br1, br2;
-
-	br1 = i * 2 + 1;
-	br2 = br1 + 1;
-	branch1 = array[br1];
-	branch2 = array[br2];
-	if (((br1 < size) && (br2 < size) &&
-		(branch1 >= branch2 && branch1 > array[i]))
-		|| ((br1 == size - 1) && branch1 > array[i]))
+	while ((left = (2 * root) + 1) <= end)
 	{
-		n = array[i];
-		array[i] = branch1;
-		array[br1] = n;
-		print_array(array, size_init);
+		swap = root;
+		right = left + 1;
+		if (array[swap] < array[left])
+			swap = left;
+		if (right <= end && array[swap] < array[right])
+			swap = right;
+		if (swap == root)
+			return;
+		temp = array[root];
+		array[root] = array[swap];
+		array[swap] = temp;
+		print_array(array, size);
+		root = swap;
 	}
-	else if ((br1 < size) && (br2 < size) &&
-		(branch2 > branch1 && branch2 > array[i]))
-	{
-		n = array[i];
-		array[i] = branch2;
-		array[br2] = n;
-		print_array(array, size_init);
-	}
-	if (br1 < size - 1)
-		check(array, size_init, size, br1);
-	if (br2 < size - 1)
-		check(array, size_init, size, br2);
 }
+
 /**
- * heap_sort - sorts an array of integers
- * in ascending order using the Heap
- * sort algorithm
- * @array: pointer of array
- * @size: size of pointer
+ * build_heap - Builds a heap from an unsorted array.
+ * @array: The array to be turned into a heap.
+ * @size: The size of the array.
  *
-**/
+ * Return: void
+ */
+void build_heap(int *array, size_t size)
+{
+	size_t parent;
+
+	for (parent = ((size - 1) - 1) / 2; 1; parent--)
+	{
+		adjust_heap(array, parent, size - 1, size);
+		if (parent == 0)
+			break;
+	}
+}
+
+/**
+ * heap_sort - Sorts an array of ints in ascending order using the Heap sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ *
+ * Return: void
+ */
 void heap_sort(int *array, size_t size)
 {
-	size_t i, size_i = size;
-	int n;
+	size_t end;
+	int temp;
 
-	if (!array)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size / 2 ; i++)
+	build_heap(array, size);
+	end = size - 1;
+	while (end > 0)
 	{
-		check(array, size_i, size, size / 2 - 1 - i);
+		temp = array[end];
+		array[end] = array[0];
+		array[0] = temp;
+		print_array(array, size);
+		end--;
+		adjust_heap(array, 0, end, size);
 	}
-	for (i = 0; i < size_i - 1; i++)
-	{
-		n = array[0];
-		array[0] = array[size - 1 - i];
-		array[size - 1 - i] = n;
-		print_array(array, size_i);
-		check(array, size_i, size - i - 1, 0);
-	}
-
 }
+
